48082b1f96fe07f35c22fbca0d4b5e91b5692392
victor gonzalez <v.gonzalezgomez.vg@gmail.com>
2022-08-18 15:30:56 -0400

    first commit

---
+++ src/components/Header.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Header = () => {
+  return (
+    <>
+    <header>
+    <h1>Galeria de imagenes con react</h1>
+    <p>aqui encontrar distintas imagenes utilizadas para practicar React</p>
+    </header>
+    </>
+  )
+}
+
+export default Header
